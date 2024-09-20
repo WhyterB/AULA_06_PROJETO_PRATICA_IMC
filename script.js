0932df4f9098fd6f71b81c1d6f5f6b29be251df9
WhyterB <matheus.borin.soares@escola.pr.gov.br>
2024-09-20 12:35:09 -0300

    praticando calculadora imc

---
+++ script.js
@@ -0,0 +1,55 @@
+//IMC
+/*ABAIXO DE 17 - MUITO ABAIXO DO PESO
+ENTRE 17 E 18,49 - ABAIXO DO PESO
+ENTRE 18,50 E 24,99 - PESO NORMAL
+ENTRE 25 E 29,99 - ACIMA DO PESO
+APARTIR DE 30 - OBESIDADE
+
+IMC = PESO/(ALTURA*ALTURA)
+*/
+
+var peso;
+var altura;
+var imc;
+var resultado;
+
+function calcular(event){
+    //alert("teste");
+    event.preventDefault();
+
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+
+    //testar a recepção das informações
+    console.log(peso);
+    console.log(altura);
+
+    //calculo do imc
+    imc = peso/(altura*altura);
+
+    console.log(imc);
+
+    if(imc<17){
+        resultado=document.getElementById('resultado');
+        resultado.innerHTML ="</br>Seu resultado foi: "+imc.toFixed(2)+"</br>Cuidado você está muito abaixo do peso"
+
+    }else if(imc>17 && imc<=18.49){
+        resultado=document.getElementById('resultado');
+        resultado.innerHTML ="</br>Seu resultado foi: "+imc.toFixed(2)+"</br>Cuidado você está abaixo do peso"
+
+    }else if(imc>18.50 && imc<=24.99){
+        resultado=document.getElementById('resultado')
+        resultado.innerHTML ="</br>Seu resultado foi: "+imc.toFixed(2)+"</br>Parabéns você está no peso ideal"
+        
+    }else if(imc>25 && imc<=29.99){
+        resultado=document.getElementById('resultado')
+        resultado.innerHTML ="</br>Seu resultado foi: "+imc.toFixed(2)+"</br>Cuidado você está acima do peso"
+
+    }else if(imc>30){
+        resultado=document.getElementById('resultado')
+        resultado.innerHTML ="</br>Seu resultado foi: "+imc.toFixed(2)+"</br>Cuidado você está extremamente obeso"
+
+    }
+
+
+}
